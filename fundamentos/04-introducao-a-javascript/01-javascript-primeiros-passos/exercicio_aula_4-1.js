7dfc732c208de974d62857ce749361e3c35c10d1
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-21 18:15:01 -0300

    Realizado exercicio 1 pós aula

---
+++ fundamentos/04-introducao-a-javascript/01-javascript-primeiros-passos/exercicio_aula_4-1.js
@@ -0,0 +1,19 @@
+// exercicio 1
+
+const a = 5;
+const b = 10;
+
+//programa 1
+let sum = a + b;
+
+//programa 2
+let sub = a -b;
+
+//programa 3
+let mult = a * b;
+
+//programa 4
+let div = a / b
+
+//programa 5
+let mod = a % b;
