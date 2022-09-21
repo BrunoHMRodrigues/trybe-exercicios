d1fb06ca546ead88428eb646b8de96c96bcbfc0a
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-21 19:15:17 -0300

    Realizado exercicio 7 pós aula

--- fundamentos/04-introducao-a-javascript/01-javascript-primeiros-passos/exercicio_aula_4-1.js
+++ fundamentos/04-introducao-a-javascript/01-javascript-primeiros-passos/exercicio_aula_4-1.js
@@ -87,3 +87,23 @@ switch (peca) {
     default:
         console.log("nome de peça inválida");
 }
+
+//exercicio 7
+
+const nota = 70;
+
+if (nota >= 90 && nota <= 100) {
+    console.log("A");
+} else if (nota >= 80) {
+    console.log("B");
+} else if (nota >= 70) {
+    console.log("C");
+} else if (nota >= 60) {
+    console.log("D");
+} else if (nota >= 50) {
+    console.log("E");
+} else if (nota < 50 && nota >= 0) {
+    console.log("F");
+} else {
+    console.log("nota inválida");
+}
