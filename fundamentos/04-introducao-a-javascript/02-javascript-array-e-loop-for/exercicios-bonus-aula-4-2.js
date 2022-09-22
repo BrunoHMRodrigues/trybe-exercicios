e55e30e57dbacfadfc4c39f36262ab5f18211c55
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-22 19:04:42 -0300

    Realizado Exercicio Bonus 02

--- fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-bonus-aula-4-2.js
+++ fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-bonus-aula-4-2.js
@@ -1,8 +1,16 @@
-// Ordene o array numbers em ordem crescente e imprima seus valores;
+// 01 - Ordene o array numbers em ordem crescente e imprima seus valores;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numbersSort = [];
 
-numbersSort = numbers.sort(function(a,b) {return a - b});
+// numbersSort = numbers.sort(function(a,b) {return a - b});
+// for (let index = 0; index < numbers.length; index+=1) {
+//     console.log(numbersSort[index]);
+    
+// }
+
+// 02 - Ordene o array numbers em ordem decrescente e imprima seus valores
+
+numbersSort = numbers.sort(function(a,b) {return b - a});
 for (let index = 0; index < numbers.length; index+=1) {
     console.log(numbersSort[index]);
     
