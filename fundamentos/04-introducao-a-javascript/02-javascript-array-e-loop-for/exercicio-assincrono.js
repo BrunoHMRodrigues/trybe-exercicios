43dab00d522df618d85441536cb7adde9cce32c6
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-22 15:42:10 -0300

    Realizado exercício assíncrono 05

--- fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicio-assincrono.js
+++ fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicio-assincrono.js
@@ -18,8 +18,15 @@
 // console.log(menu);
 
 // Utilize o for para imprimir os elementos da lista groceryList com o console.log():
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-for (let index = 0; index < groceryList.length; index+=1) {
-    console.log(groceryList[index]);    
+// for (let index = 0; index < groceryList.length; index+=1) {
+//     console.log(groceryList[index]);    
+// }
+
+// Utilize o for/of para imprimir os elementos da lista names com o console.log():
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (const name of names) {
+    console.log(name);
 }
