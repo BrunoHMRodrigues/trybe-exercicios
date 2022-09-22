d6b9352fbf5a695381b7107fc3e3a58630693767
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-22 15:49:46 -0300

    Realizado subitem 02 do exercicio foguetinho 01

--- fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-aula-4-2.js
+++ fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-aula-4-2.js
@@ -4,6 +4,15 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
+// for (const number of numbers) {
+//     console.log(number);
+// }
+
+// Some todos os valores contidos no array e imprima o resultado;
+let sum = 0;
+
 for (const number of numbers) {
-    console.log(number);
+    sum = sum + number;
 }
+
+console.log(sum);
