ac683803b3877790af37370f6c8188ec1f675729
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-22 19:13:40 -0300

    Realizado Exercicio Bonus 03

--- fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-bonus-aula-4-2.js
+++ fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-bonus-aula-4-2.js
@@ -10,8 +10,22 @@ let numbersSort = [];
 
 // 02 - Ordene o array numbers em ordem decrescente e imprima seus valores
 
-numbersSort = numbers.sort(function(a,b) {return b - a});
-for (let index = 0; index < numbers.length; index+=1) {
-    console.log(numbersSort[index]);
+// numbersSort = numbers.sort(function(a,b) {return b - a});
+// for (let index = 0; index < numbers.length; index+=1) {
+//     console.log(numbersSort[index]);
     
+// }
+
+// 03 - Agora, crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
+let numerosMultiplicados = [];
+
+for (let index = 0; index < numbers.length; index+=1) {
+    if (index < numbers.length - 1) {
+        numerosMultiplicados[index] = numbers[index] * numbers[index+1];
+    } else {
+        numerosMultiplicados[index] = numbers[index] * 2;
+    }  
+    console.log(numerosMultiplicados[index]);
 }
+
+
