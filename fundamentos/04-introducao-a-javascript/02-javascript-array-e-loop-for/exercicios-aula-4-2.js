b44fe7db220c472cb059711e2d1cbbbaa4ba747a
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-22 16:00:17 -0300

    Realizado subitem 06 do exercicio foguetinho 01

--- fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-aula-4-2.js
+++ fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-aula-4-2.js
@@ -2,13 +2,13 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+// 01 - Percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
 // for (const number of numbers) {
 //     console.log(number);
 // }
 
-// Some todos os valores contidos no array e imprima o resultado;
+// 02 - Some todos os valores contidos no array e imprima o resultado;
 // let sum = 0;
 
 // for (const number of numbers) {
@@ -17,7 +17,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(sum);
 
-// Calcule e imprima a média aritmética dos valores contidos no array;
+// 03 - Calcule e imprima a média aritmética dos valores contidos no array;
 // A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
 // let sum = 0;
 // let mediaAritmetica = 0;
@@ -30,7 +30,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(mediaAritmetica);
 
-// Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”
+// 04 - Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”
 
 // if (mediaAritmetica > 20) {
 //     console.log("valor maior que 20");
@@ -38,13 +38,24 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     console.log("valor menor ou igual a 20");
 // }
 
-// Utilizando for, descubra qual o maior valor contido no array e imprima-o
-let maiorValor = 0;
+// 05 - Utilizando for, descubra qual o maior valor contido no array e imprima-o
+// let maiorValor = 0;
+
+// for (const number of numbers) {
+//     if (number > maiorValor) {
+//         maiorValor = number
+//     }
+// }
+
+// console.log(maiorValor);
+
+// 06 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”
+let contadorImpar = 0;
 
 for (const number of numbers) {
-    if (number > maiorValor) {
-        maiorValor = number
+    if (number % 2 === 1) {
+        contadorImpar = contadorImpar +=1
     }
 }
 
-console.log(maiorValor);
+console.log(contadorImpar);
