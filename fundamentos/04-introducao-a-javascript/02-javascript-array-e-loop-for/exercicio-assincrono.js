3fda8f79e3568d1ee09214a97884d7d66767660b
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-22 14:49:50 -0300

    Realizado exercício assíncrono 03

--- fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicio-assincrono.js
+++ fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicio-assincrono.js
@@ -5,7 +5,14 @@
 // console.log(menuServices);
 
 // Procure o índice do valor “Portfólio” do array menu:
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu.indexOf('Portfólio');
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
 
-console.log(indexOfPortfolio);
+// console.log(indexOfPortfolio);
+
+// Adicione o valor “Contato” no final do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push("Contato");
+
+console.log(menu);
