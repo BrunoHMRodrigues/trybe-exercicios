bc8f497387721a2adcb8a27351650dad841e6e98
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-22 15:57:23 -0300

    Realizado subitem 05 do exercicio foguetinho 01

--- fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-aula-4-2.js
+++ fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-aula-4-2.js
@@ -19,21 +19,32 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Calcule e imprima a média aritmética dos valores contidos no array;
 // A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
-let sum = 0;
-let mediaAritmetica = 0;
+// let sum = 0;
+// let mediaAritmetica = 0;
 
-for (const number of numbers) {
-     sum = sum + number;
-}
+// for (const number of numbers) {
+//      sum = sum + number;
+// }
 
-mediaAritmetica = sum / numbers.length;
+// mediaAritmetica = sum / numbers.length;
 
-console.log(mediaAritmetica);
+// console.log(mediaAritmetica);
 
 // Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”
 
-if (mediaAritmetica > 20) {
-    console.log("valor maior que 20");
-} else {
-    console.log("valor menor ou igual a 20");
+// if (mediaAritmetica > 20) {
+//     console.log("valor maior que 20");
+// } else {
+//     console.log("valor menor ou igual a 20");
+// }
+
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o
+let maiorValor = 0;
+
+for (const number of numbers) {
+    if (number > maiorValor) {
+        maiorValor = number
+    }
 }
+
+console.log(maiorValor);
