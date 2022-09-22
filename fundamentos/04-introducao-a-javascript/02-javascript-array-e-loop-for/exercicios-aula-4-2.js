3e45a07250497a263350767247afd85c3e5bdd92
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-22 15:52:32 -0300

    Realizado subitem 03 do exercicio foguetinho 01

--- fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-aula-4-2.js
+++ fundamentos/04-introducao-a-javascript/02-javascript-array-e-loop-for/exercicios-aula-4-2.js
@@ -9,10 +9,23 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // Some todos os valores contidos no array e imprima o resultado;
+// let sum = 0;
+
+// for (const number of numbers) {
+//     sum = sum + number;
+// }
+
+// console.log(sum);
+
+// Calcule e imprima a média aritmética dos valores contidos no array;
+// A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
 let sum = 0;
+let mediaAritmetica = 0;
 
 for (const number of numbers) {
     sum = sum + number;
 }
 
-console.log(sum);
+mediaAritmetica = sum / numbers.length;
+
+console.log(mediaAritmetica);
