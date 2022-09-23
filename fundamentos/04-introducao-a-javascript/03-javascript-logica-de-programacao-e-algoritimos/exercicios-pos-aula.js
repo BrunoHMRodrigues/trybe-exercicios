c026fbe6162f62f01af69c9c51e89a890a888819
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-23 18:29:48 -0300

    Realizado exercicio 2 dos exercicios pós aula

--- fundamentos/04-introducao-a-javascript/03-javascript-logica-de-programacao-e-algoritimos/exercicios-pos-aula.js
+++ fundamentos/04-introducao-a-javascript/03-javascript-logica-de-programacao-e-algoritimos/exercicios-pos-aula.js
@@ -1,8 +1,19 @@
 // Exercicio 1
 // crie um algoritmo que imprima na tela o fatorial de 10.
-let fatorial = 1;
+// let fatorial = 1;
 
-for (let index = 1; index <= 10; index+=1) {
-    fatorial = fatorial * index;
+// for (let index = 1; index <= 10; index+=1) {
+//     fatorial = fatorial * index;
+// }
+// console.log(fatorial);
+
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber';
+let newWord = "";
+
+for (let index = word.length - 1; index >= 0; index-=1) {
+    newWord = newWord + word[index];
 }
-console.log(fatorial);
+
+console.log(newWord);
