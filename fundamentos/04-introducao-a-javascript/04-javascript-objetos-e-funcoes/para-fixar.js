ef0a2b16317b042a98bbff35246e06a6a37a0f95
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-26 18:30:19 -0300

    Realizado Exercicio 05 pós aula sobre Funções

--- fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
+++ fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
@@ -210,6 +210,21 @@ let info = {
 
 // Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
 
+// for (const key in info) {
+//     console.log(info[key]);
+// }
+
+// Exercicio 05
+
+// Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
 for (const key in info) {
-    console.log(info[key]);
+    console.log(info[key] + " e " + info2[key]);
 }
