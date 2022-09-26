e205be9b4af0baa9dfeea955bbb4b5a1d8d2364f
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-26 18:16:34 -0300

    Realizado Exercicio 02 pós aula sobre Funções

--- fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
+++ fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
@@ -172,13 +172,28 @@
 
 // Exercicio 01
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
   // Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console:
   // Bem-vinda, Margarida
 
-  console.log("Bem-vinda, " + info.personagem);
+//   console.log("Bem-vinda, " + info.personagem);
+
+  // Exercicio 02
+
+//   Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+};
+
+info['recorrente'] = "Sim";
+
+console.log(info);
