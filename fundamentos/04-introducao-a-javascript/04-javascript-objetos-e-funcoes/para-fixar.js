9a4e3650332409ebbc9451c8098a6ae42814f563
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-26 18:47:46 -0300

    Realizado Exercicio 06 pós aula (Foguetinho subitem 01) sobre Funções

--- fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
+++ fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
@@ -187,12 +187,12 @@
 
 //   Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-};
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: 'Sim'
+// };
 
 // info['recorrente'] = "Sim";
 
@@ -218,13 +218,34 @@ let info = {
 
 // Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
-};
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim'
+// };
 
-for (const key in info) {
-    console.log(info[key] + " e " + info2[key]);
-}
+// for (const key in info) {
+//     console.log(info[key] + " e " + info2[key]);
+// }
+
+// Exercicio Foguetinho 01
+
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  // Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+
+  console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + leitor.livrosFavoritos[0].titulo);
