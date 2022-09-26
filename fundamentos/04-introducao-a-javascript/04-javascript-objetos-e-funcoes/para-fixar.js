dc692b8bcb15dbf1a2f1cf55884da02ae30aba04
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-26 15:35:51 -0300

    Realizado Exercicio 01 para fixar sobre For/in e For/of

--- fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
+++ fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
@@ -8,13 +8,30 @@
 // let age = 34;
 // let medals = { golden: 2, silver: 3 };
 
-let player = {
-    name: "Silva",
-    age: 34,
-    medals: {
-        golden: 2,
-        silver: 3
-    }
-}
+// let player = {
+//     name: "Silva",
+//     age: 34,
+//     medals: {
+//         golden: 2,
+//         silver: 3
+//     }
+// }
 
-console.log(player);
+// console.log(player);
+
+// For/in e For/of
+//Exercicio 01
+
+
+// Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for (const key in names) {    
+        console.log("Olá " + names[key]);        
+  }
+  
