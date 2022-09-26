2dcb0989ff23edd439453c2f3343db6606fcc343
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-26 19:20:25 -0300

    Realizado Exercicio 07 pós aula Funções

--- fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
+++ fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
@@ -233,18 +233,18 @@
 
 // Usando o objeto abaixo, faça os exercícios a seguir:
 
-let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-    ],
-  };
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+//       },
+//     ],
+//   };
 
   // Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
 
@@ -252,14 +252,44 @@ let leitor = {
 
 // Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto contendo as seguintes informações:
 
-leitor.livrosFavoritos[1] = {
-    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-    autor: 'JK Rowling',
-    editora: 'Rocco',
+// leitor.livrosFavoritos[1] = {
+//     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//     autor: 'JK Rowling',
+//     editora: 'Rocco',
+// }
+
+// // console.log(leitor);
+
+// // Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código.
+
+// console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos");
+
+// Funções
+
+// Exercicio 01
+
+// Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
+
+// Exemplo de palíndromo: arara
+
+// verificaPalindrome('arara')
+
+// Retorno esperado: true
+// verificaPalindrome('desenvolvimento')
+
+// Retorno esperado: false
+let palavra = "arare";
+
+function ePalindromo (palavra) {
+    let arrayPalavra = palavra.split('').join();
+    let palavraReversa = palavra.split('').reverse().join();
+
+    if (arrayPalavra === palavraReversa) {
+        return true
+    } else {
+        return false
+    }
 }
 
-// console.log(leitor);
 
-// Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código.
-
-console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos");
+console.log(ePalindromo(palavra));
