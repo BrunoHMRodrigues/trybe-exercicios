30389b7f16cc27f2224723ff36771fc0e0441d34
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-26 23:08:14 -0300

    Realizado Exercicio 11 pós aula Funções

--- fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
+++ fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/para-fixar.js
@@ -349,17 +349,52 @@
 
 // Valor esperado no retorno da função: Fernanda.
 
-let arrayNomes =  ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+// let arrayNomes =  ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
-function maiorNome(nomes) {
-    let maiorNome = nomes[0];
+// function maiorNome(nomes) {
+//     let maiorNome = nomes[0];
 
-    for (let index = 0; index < nomes.length; index+=1) {
-        if (nomes[index].length > maiorNome.length) {
-            maiorNome = nomes[index];
+//     for (let index = 0; index < nomes.length; index+=1) {
+//         if (nomes[index].length > maiorNome.length) {
+//             maiorNome = nomes[index];
+//         }
+//     }
+//     return maiorNome
+// }
+
+// console.log(maiorNome(arrayNomes));
+
+// Exercicio 05
+
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+// Array de teste: [2, 3, 2, 5, 8, 2, 3];.
+
+// Valor esperado no retorno da função: 2.
+
+let array = [2, 3, 2, 5, 8, 2, 3];
+
+function contRepetido(array) {    
+    let maisRepetido = array [0];
+    let maiorContador = null;
+
+    for (let index = 0; index < array.length; index+=1) {
+        let contador = 0;
+        let numero = array[index];
+
+        for (let index2 = 0; index2 < array.length; index2 +=1) {
+            if (array[index2] == numero) {
+                contador +=1;
+            }            
         }
+
+        if (contador > maiorContador) {
+            maisRepetido = array[index];
+            maiorContador = contador;
+        }
+        
     }
-    return maiorNome
+    return maisRepetido;
 }
 
-console.log(maiorNome(arrayNomes));
+console.log(contRepetido(array));
