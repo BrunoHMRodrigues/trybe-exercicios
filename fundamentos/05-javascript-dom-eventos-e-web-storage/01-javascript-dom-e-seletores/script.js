cf4573b17d1113f2702f222ebe6ff75d68c83cc6
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-28 15:23:53 -0300

    Realizado exercicio 03 do conteúdo assíncrono

--- fundamentos/05-javascript-dom-eventos-e-web-storage/01-javascript-dom-e-seletores/script.js
+++ fundamentos/05-javascript-dom-eventos-e-web-storage/01-javascript-dom-e-seletores/script.js
@@ -1,6 +1,13 @@
 // Exercicio conteúdo assíncrono 01
+// Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
 
 document.getElementById("page-title").innerText = "Rei Leão";
 
 // Exercicio conteúdo assíncrono 02
+// Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
 document.getElementById("second-paragraph").innerText = "Távola Redonda";
+
+// Exercicio conteúdo assíncrono 03
+// Por fim, recupere o subtítulo e altere-o também.
+
+document.getElementById("subtitle").innerText = "Alterando subtítulo";
