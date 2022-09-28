fbc2415432546cf65a2a3d0da9b2a48f4aab1fdf
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-28 15:22:21 -0300

    Realizado exercicio 02 do conteúdo assíncrono

--- fundamentos/05-javascript-dom-eventos-e-web-storage/01-javascript-dom-e-seletores/script.js
+++ fundamentos/05-javascript-dom-eventos-e-web-storage/01-javascript-dom-e-seletores/script.js
@@ -1,3 +1,6 @@
 // Exercicio conteúdo assíncrono 01
 
 document.getElementById("page-title").innerText = "Rei Leão";
+
+// Exercicio conteúdo assíncrono 02
+document.getElementById("second-paragraph").innerText = "Távola Redonda";
