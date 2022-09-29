91d6b25808e1a6813368eb41cdd67b0004d96d92
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-29 19:00:37 -0300

    Realizado exercicio até o número 6  conteúdo assíncrono

--- fundamentos/05-javascript-dom-eventos-e-web-storage/02-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/05-javascript-dom-eventos-e-web-storage/02-javascript-trabalhando-com-elementos/script.js
@@ -1,26 +1,29 @@
 // Exercicio 1
 // Acesse o elemento elementoOndeVoceEsta.
-const elementoOndeVoceEsta = document.getElementById("primeiroFilho").nextElementSibling;
+const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
 
 
 // Exercicio 2
 // Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 const pai = elementoOndeVoceEsta.parentElement;
-pai.style.color = "blue";
+pai.style.color = "green";
 
 // Exercicio 3
 // Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
-const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild
-primeiroFilhoDoFilho.innertext = "Vamo que Vamo!";
+const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
+primeiroFilhoDoFilho.innerText = 'Vamo que vamo';
 
 // Exercicio 4
 // Acesse o primeiroFilho a partir de pai.
+const primeiroFilho = pai.firstElementChild;
 
 // Exercicio 5
 // Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+const primeiroFilhoCaminho2 = elementoOndeVoceEsta.previousElementSibling;
 
 // Exercicio 6
 // Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+const text = elementoOndeVoceEsta.nextSibling;
 
 // Exercicio 7
 // Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
