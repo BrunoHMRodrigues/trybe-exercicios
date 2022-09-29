59c0caa5e54a1c1856aade36b0f526ff46964cb5
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-09-29 15:13:59 -0300

    Realizado exercicio 01 conteúdo assíncrono

---
+++ fundamentos/05-javascript-dom-eventos-e-web-storage/02-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,23 @@
+// Exercicio 1
+// Acesse o elemento elementoOndeVoceEsta.
+document.getElementById("primeiroFilho").nextElementSibling;
+
+// Exercicio 2
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+// Exercicio 3
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+
+// Exercicio 4
+// Acesse o primeiroFilho a partir de pai.
+
+// Exercicio 5
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+
+// Exercicio 6
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+
+// Exercicio 7
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+
+// Exercicio 8
+// Agora acesse o terceiroFilho a partir de pai.
