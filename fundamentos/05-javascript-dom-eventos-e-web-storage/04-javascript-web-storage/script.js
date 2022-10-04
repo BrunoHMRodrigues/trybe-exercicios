871cb43ae8673c5b08deb5a8efc9bc1fa649ddef
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-04 00:44:39 -0300

    Realizado lógica mudança de cor fonte quando botões são apertados

--- fundamentos/05-javascript-dom-eventos-e-web-storage/04-javascript-web-storage/script.js
+++ fundamentos/05-javascript-dom-eventos-e-web-storage/04-javascript-web-storage/script.js
@@ -16,3 +16,22 @@ function changeBackgroundColor (event) {
 backgroundColorButton1.addEventListener("click", changeBackgroundColor);
 backgroundColorButton2.addEventListener("click", changeBackgroundColor);
 backgroundColorButton3.addEventListener("click", changeBackgroundColor);
+
+
+const fontColorButton1 = document.getElementById("button-text-color1");
+const fontColorButton2 = document.getElementById("button-text-color2");
+const fontColorButton3 = document.getElementById("button-text-color3");
+const paragraph = document.getElementById("paragraph-text");
+
+function changeFontColor (event) {
+    if (event.target === fontColorButton1) {
+        paragraph.style.color = "black";        
+    } else if (event.target === fontColorButton2) {
+        paragraph.style.color = "red";        
+    } else if (event.target === fontColorButton3) {
+        paragraph.style.color = "green";        
+    }
+}
+fontColorButton1.addEventListener("click", changeFontColor);
+fontColorButton2.addEventListener("click", changeFontColor);
+fontColorButton3.addEventListener("click", changeFontColor);
