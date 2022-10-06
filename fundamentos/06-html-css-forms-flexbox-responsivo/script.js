993a1987704dbd59eda35bcdc99d3b98deb58ad9
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-06 15:54:51 -0300

    Realizado Requisito 01 atividade assíncrona prevent default

--- fundamentos/06-html-css-forms-flexbox-responsivo/script.js
+++ fundamentos/06-html-css-forms-flexbox-responsivo/script.js
@@ -2,3 +2,12 @@
 const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
+
+
+const href = document.getElementById("href");
+
+function clicarLink(event) {
+    event.preventDefault()
+}
+
+href.addEventListener("click", clicarLink);
