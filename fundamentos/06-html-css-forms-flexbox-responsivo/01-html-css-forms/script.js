f84c819b4a9de0975ce433d27839dcc0f50312a2
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-10 15:03:12 -0300

    Atualizando branch

--- fundamentos/06-html-css-forms-flexbox-responsivo/01-html-css-forms/script.js
+++ fundamentos/06-html-css-forms-flexbox-responsivo/01-html-css-forms/script.js
@@ -1,6 +1,11 @@
 const submit = document.querySelector("#submit");
 
 function submitData (event) {
-    event.preventDefault()
+    // event.preventDefault()
+
+    const name = document.querySelector("#userName");
+    if (name.ariaValueMax.length < 10) {
+        alert("Dados Inválidos")
+    }
 }
 submit.addEventListener("click", submitData)
