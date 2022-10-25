f5a2a2084c9e720ef220853ea9af9e9fa9a54f3f
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-25 16:01:48 -0300

    Realizando exercicio assíncrono 06 Seção 8 Aula 2

--- fundamentos/08-higher-order-functions-javascript-es6/02-higher-order-functions-foreach-find-some-sort/script.js
+++ fundamentos/08-higher-order-functions-javascript-es6/02-higher-order-functions-foreach-find-some-sort/script.js
@@ -59,14 +59,27 @@
   
 //   console.log(findMusic('31031685'));
 
-const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+// const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
-const hasName = (arr, name) => {
-  return arr.some( (element) => {
-    if (element === name) {
-        return true;
-    }
-  })
-};
+// const hasName = (arr, name) => {
+//   return arr.some( (element) => {
+//     if (element === name) {
+//         return true;
+//     }
+//   })
+// };
 
-console.log(hasName(names, 'Ana'));
+// console.log(hasName(names, 'Ana'));
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  people.sort( (a, b) => a.age - b.age);
+  // Adicione se código aqui
+  
+  console.log(people);
