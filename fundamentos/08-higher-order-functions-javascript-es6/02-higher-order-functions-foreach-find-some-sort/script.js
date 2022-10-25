930f97004a3cdac9b8a95863630a283e575e778c
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-25 15:16:35 -0300

    Realizando exercicio assíncrono 02 Seção 8 Aula 2

--- fundamentos/08-higher-order-functions-javascript-es6/02-higher-order-functions-foreach-find-some-sort/script.js
+++ fundamentos/08-higher-order-functions-javascript-es6/02-higher-order-functions-foreach-find-some-sort/script.js
@@ -1,12 +1,21 @@
-const emailListInData = [
-    'roberta@email.com',
-    'paulo@email.com',
-    'anaroberta@email.com',
-    'fabiano@email.com',
-  ];
+// const emailListInData = [
+//     'roberta@email.com',
+//     'paulo@email.com',
+//     'anaroberta@email.com',
+//     'fabiano@email.com',
+//   ];
   
-  const showEmailList = (email) => {
-    console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
-  };
+//   const showEmailList = (email) => {
+//     console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+//   };
   
-  emailListInData.forEach(showEmailList);
+//   emailListInData.forEach(showEmailList);
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (number) => {    
+  if (number % 3 === 0 && number % 5 === 0) {
+    return true;
+  }
+};
+console.log(numbers.find(findDivisibleBy3And5));
