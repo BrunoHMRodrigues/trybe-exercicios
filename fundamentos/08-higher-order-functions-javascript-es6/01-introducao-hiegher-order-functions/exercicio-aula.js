425184954ac5a2afbbb16e4c4199cad6efdf1fd9
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-25 10:08:42 -0300

    Realizado Exercicio 01 pós aula Seção 8 Aula 01

--- fundamentos/08-higher-order-functions-javascript-es6/01-introducao-hiegher-order-functions/exercicio-aula.js
+++ fundamentos/08-higher-order-functions-javascript-es6/01-introducao-hiegher-order-functions/exercicio-aula.js
@@ -1,8 +1,18 @@
-const newEmployees = () => {
+const dataPerson = (completedName) => {
+    const createEmail = completedName.replace(/\s/g, '').toLowerCase();
+    const objectPerson = {
+        nomeCompleto: completedName,
+        email: `${createEmail}@trybe.com`,
+    }
+    return objectPerson;
+};
+const newEmployees = (completedName) => {
     const employees = {
-      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: dataPerson('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: dataPerson('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: dataPerson('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
   };
+
+  console.log(newEmployees(dataPerson));
