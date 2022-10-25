dc9f8388232155a26f3e27f8618844501f51b512
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-25 15:30:03 -0300

    Realizando exercicio assíncrono 04 Seção 8 Aula 2

--- fundamentos/08-higher-order-functions-javascript-es6/02-higher-order-functions-foreach-find-some-sort/script.js
+++ fundamentos/08-higher-order-functions-javascript-es6/02-higher-order-functions-foreach-find-some-sort/script.js
@@ -20,11 +20,41 @@
 // };
 // console.log(numbers.find(findDivisibleBy3And5));
 
-const names = ['João', 'Irene', 'Fernando', 'Maria'];
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
-const findNameWithFiveLetters = (word) => {
-  if (word.length === 5) {
-    return true;
-  }
-};
-console.log(names.find(findNameWithFiveLetters));
+// const findNameWithFiveLetters = (word) => {
+//   if (word.length === 5) {
+//     return true;
+//   }
+// };
+// console.log(names.find(findNameWithFiveLetters));
+
+// const musicas = [
+//     { id: '31031685', title: 'Partita in C moll BWV 997' },
+//     { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+//     { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+//   ];
+  
+//   function findMusic(element) {
+//     if (element.id === '31031685') {
+//         return true;
+//     }
+//   };
+  
+//   console.log(musicas.find(findMusic));
+
+  const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ];
+  
+  function findMusic(id) {
+    return musicas.find( (element) => {
+        if (element.id === id) {
+            return true;
+        }
+    });
+  };
+  
+  console.log(findMusic('31031685'));
