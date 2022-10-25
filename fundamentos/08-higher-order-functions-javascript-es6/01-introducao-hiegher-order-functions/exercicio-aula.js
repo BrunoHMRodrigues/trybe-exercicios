b841d01c480bed7f2f8c7f7ac41bbaa444ea5e5b
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-25 10:50:48 -0300

    Realizado Exercicio 03 pós aula Seção 8 Aula 01

--- fundamentos/08-higher-order-functions-javascript-es6/01-introducao-hiegher-order-functions/exercicio-aula.js
+++ fundamentos/08-higher-order-functions-javascript-es6/01-introducao-hiegher-order-functions/exercicio-aula.js
@@ -16,21 +16,44 @@
 //   };
 
 //   console.log(newEmployees(dataPerson));
-const randomGame = (betNumber, randomNumber) => {
-    console.log('betnumber: ' + betNumber);
-    console.log('random number: ' + randomNumber);
-    const result = (betNumber === randomNumber) ? 'Parabéns você ganhou' : 'Tente novamente';
-    // let result = '';
-    // if (betNumber === randomNumber) {
-    //     result = 'Parabéns você ganhou';
-    // } else {
-    //     result = 'Tente novamente';
-    // }
-    return result;
+
+// const randomGame = (betNumber, randomNumber) => {
+//     console.log('betnumber: ' + betNumber);
+//     console.log('random number: ' + randomNumber);
+//     const result = (betNumber === randomNumber) ? 'Parabéns você ganhou' : 'Tente novamente';
+//     // let result = '';
+//     // if (betNumber === randomNumber) {
+//     //     result = 'Parabéns você ganhou';
+//     // } else {
+//     //     result = 'Tente novamente';
+//     // }
+//     return result;
+// };
+
+// const result = (betNumber, callback) => {
+//     const randomNumber = Math.floor(Math.random() * 5 + 1);
+//     return callback(betNumber, randomNumber);    
+// };
+// console.log(result(3, randomGame));
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const verifyPontuation = (rightAnswer, studentAnswer) => {
+    let totalPontuation = 0;
+    for (let index = 0; index < rightAnswer.length; index += 1) {
+        if (rightAnswer[index] === studentAnswer[index]) {
+            totalPontuation += 1;
+        } else if (studentAnswer[index] === 'N.A') {
+
+        } else {
+            totalPontuation -= 0.5;
+        }        
+    }
+    return totalPontuation;
 };
 
-const result = (betNumber, callback) => {
-    const randomNumber = Math.floor(Math.random() * 5 + 1);
-    return callback(betNumber, randomNumber);    
+const resultTest = (RIGHT_ANSWERS, STUDENT_ANSWERS, callback) => {
+    return callback(RIGHT_ANSWERS, STUDENT_ANSWERS);
 };
-console.log(result(3, randomGame));
+console.log(resultTest(RIGHT_ANSWERS, STUDENT_ANSWERS, verifyPontuation));
