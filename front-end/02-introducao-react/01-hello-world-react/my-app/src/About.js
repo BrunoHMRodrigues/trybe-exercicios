27bd1f0a76e60a79912563833e8e3382ddd3c152
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-11-24 10:26:15 -0300

    Realizado exercicio fixação

---
+++ front-end/02-introducao-react/01-hello-world-react/my-app/src/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class About extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Bruno</h1>
+                <p>descrição</p>
+                <h2>Minhas Habilidades</h2>
+                <ul>
+                    <li>Criatividade</li>
+                    <li>Responsabilidade</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default About;
