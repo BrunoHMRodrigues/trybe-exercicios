49a868e25a7133512d88e6eabd7623353a3b9c83
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-12-01 10:56:20 -0300

    Realizado exercicio de fixação 02

--- front-end/03-componentes-estado-eventos-formularios-react/01-componentes-com-estado-e-eventos/exercicios-fixacao/fancy-buttons/src/App.js
+++ front-end/03-componentes-estado-eventos-formularios-react/01-componentes-com-estado-e-eventos/exercicios-fixacao/fancy-buttons/src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import React from 'react';
 
-function handleClick(event) {
-  const button = event.target.name
-  console.log(`clicou no botão: ${button}`);
-}
-
 class App extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(event) {
+    const button = event.target.name
+    console.log(`clicou no botão: ${button}`);
+  }
+
   render() {
     return (
       <main>
-        <button name="Botão1" onClick={handleClick}>Clique Aqui</button>
-        <button name="Botão2" onClick={handleClick}>Clique Aqui</button>
-        <button name="Botão3" onClick={handleClick}>Clique Aqui</button>
+        <button name="Botão1" onClick={this.handleClick}>Clique Aqui</button>
+        <button name="Botão2" onClick={this.handleClick}>Clique Aqui</button>
+        <button name="Botão3" onClick={this.handleClick}>Clique Aqui</button>
       </main>
     )
   }
