4bd29b9dac00300ec74ac9eec53d6a4363306c80
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-12-01 11:34:42 -0300

    Realizado exercicio de fixação 04

--- front-end/03-componentes-estado-eventos-formularios-react/01-componentes-com-estado-e-eventos/exercicios-fixacao/fancy-buttons/src/App.js
+++ front-end/03-componentes-estado-eventos-formularios-react/01-componentes-com-estado-e-eventos/exercicios-fixacao/fancy-buttons/src/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
     this.handleClickBtn1 = this.handleClickBtn1.bind(this);
     this.handleClickBtn2 = this.handleClickBtn2.bind(this);
     this.handleClickBtn3 = this.handleClickBtn3.bind(this);
+    this.getButtonColor = this.getButtonColor.bind(this);
 
     this.state = {
       clicksBtn1: 0,
@@ -22,29 +23,42 @@ class App extends React.Component {
   //   clicksBtn3: 0,
   // };
 
-  handleClickBtn1() {
-    this.setState((previewState, _props) => ({
+  handleClickBtn1(event) {
+    this.setState((previewState) => ({
       clicksBtn1: previewState.clicksBtn1 + 1,
-    }))
+    }), () => {
+      const color = event.target.style.backgroundColor;
+      console.log(color);
+    })
   };
-  handleClickBtn2() {
-    this.setState((previewState, _props) => ({
+  handleClickBtn2(event) {
+    this.setState((previewState) => ({
       clicksBtn2: previewState.clicksBtn2 + 1,
-    }))
+    }), () => {
+      const color = event.target.style.backgroundColor;
+      console.log(color);
+    })
   };
-  handleClickBtn3() {
-    this.setState((previewState, _props) => ({
+  handleClickBtn3(event) {
+    this.setState((previewState) => ({
       clicksBtn3: previewState.clicksBtn3 + 1,
-    }))
+    }), () => {
+      const color = event.target.style.backgroundColor;
+      console.log(color);
+    })
   };
+
+  getButtonColor(number) {
+    return (number % 2 === 0) ? 'green' : 'blue';
+  }
 
   render() {
     const { clicksBtn1, clicksBtn2, clicksBtn3 } = this.state;
     return (
       <main>
-        <button name="Botão1" onClick={this.handleClickBtn1}>{`Botão 1, Clicks: ${clicksBtn1}`}</button>
-        <button name="Botão2" onClick={this.handleClickBtn2}>{`Botão 2, Clicks: ${clicksBtn2}`}</button>
-        <button name="Botão3" onClick={this.handleClickBtn3}>{`Botão 3, Clicks: ${clicksBtn3}`}</button>
+        <button name="Botão1" style={ { backgroundColor: this.getButtonColor(clicksBtn1) } } onClick={this.handleClickBtn1}>{`Botão 1, Clicks: ${clicksBtn1}`}</button>
+        <button name="Botão2" style={ { backgroundColor: this.getButtonColor(clicksBtn2) } } onClick={this.handleClickBtn2}>{`Botão 2, Clicks: ${clicksBtn2}`}</button>
+        <button name="Botão3" style={ { backgroundColor: this.getButtonColor(clicksBtn3) } } onClick={this.handleClickBtn3}>{`Botão 3, Clicks: ${clicksBtn3}`}</button>
       </main>
     )
   }
